fc7dc0ce7cfccfd7db66a2b91ffc35a4146f5ccf
Nidhi <nidgoyal1@gmail.com>
2017-12-04 04:38:55 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+var a, b
+
+beforeEach(function() {
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+})
+
+function add(){
+  return()
+}
+function subtract(a,b){}
+function multiply(a,b){}
+function divide(a,b){}
